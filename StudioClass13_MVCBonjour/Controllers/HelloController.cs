fe6676a43b8952998ac85b81ea3edba00ec4f480
dwmoeller1 <dwmoeller1@aol.com>
2019-04-17 20:00:01 -0500

    In class work

---
+++ StudioClass13_MVCBonjour/Controllers/HelloController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudioClass13_MVCBonjour.Controllers
+{
+    public class HelloController : Controller
+    {
+        private string htmlForm = @"
+            <form method='post'>
+                <input name='userName' />
+                <select name='language'>
+                    <option>English</option>
+                    <option>Russian</option>
+                    <option>Spanish</option>
+                    <option>French</option>
+                </select>
+                <button type='submit'>Greet me!</button>
+            </form>";
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content(htmlForm, "text/html");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string userName, string language)
+        {
+            return RedirectToAction(actionName: nameof(Greeting), routeValues: new { userName, language });
+        }
+
+        public IActionResult Greeting(string userName, string language)
+        {
+            string greeting = "";
+            if (language.ToLower() == "english")
+                greeting = "Hello";
+            if (language.ToLower() == "russian")
+                greeting = "Priviet";
+            if (language.ToLower() == "spanish")
+                greeting = "Hola";
+            if (language.ToLower() == "french")
+                greeting = "Bonjour";
+
+            return Content($"{greeting} {userName}!");
+        }
+    }
+}
